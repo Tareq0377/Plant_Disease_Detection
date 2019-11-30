90a75dffe17a2b4592ca35b2cbf176e5cdd1c2e4
Md. Ahsan Habib Rabbi <49279039+ahsanhabibrabbi@users.noreply.github.com>
2019-11-30 22:33:57 +0600

    convert image to arrays

--- Habib/plant_disease_test.py
+++ Habib/plant_disease_test.py
@@ -12,33 +12,7 @@ from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 from keras.preprocessing import image
-import keras.preprocessing.image
+from keras.preprocessing.image import img_to_array
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-
-EPOCHS = 25
-INIT_LR = 1e-3
-BS = 32
-default_image_size = tuple((256, 256))
-image_size = 0
-directory_root = '../input/plantvillage/'
-width = 256
-height = 256
-depth = 3
-
-
-
-def convert_image_to_array(image_dir):
-    try:
-        image = cv2.imread(image_dir)
-        if image is not None:
-            image = cv2.resize(image, default_image_size)
-            return keras.preprocessing.image.img_to_array(image)
-        else:
-            return np.array([])
-    except Exception as e:
-        print(f"Error : {e}")
-        return None
-
-
